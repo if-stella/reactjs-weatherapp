b7dbaca3bac013d43e8ae1d916c807c7698708f8
Stella Raab <hallo@stellaraab.de>
2022-09-19 20:55:40 +0200

    Fixed responsive issues, for mobile devices

--- src/App.js
+++ src/App.js
@@ -76,14 +76,13 @@ function App() {
   return (
     <div className="app" style={divStyle}>
       <div className="container">
-        <div className="search">
           <input
-          value={location}
-          onChange={event => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder='Enter a location'
-          type="text" />
-        </div>
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter a location...'
+            type="text"
+          />
         {data.main ?
         <div className="top-container">
           <div className="top">
@@ -124,12 +123,12 @@ function App() {
             </div>
           </div> : null}
           {data.main ?
-          <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-          viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+          <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
           <defs>
           <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
           </defs>
-          <g class="parallax">
+          <g className="parallax">
           <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.25" />
           <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.25)" />
           <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.25)" />
